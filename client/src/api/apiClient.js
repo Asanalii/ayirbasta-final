ab17eaf97c976fcd6e742b7cbbfe8d714bb8408a
Asanalii <asanaliiospan@gmail.com>
2025-05-14 19:08:31 +0500

    fix: changed url port of api

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -3,7 +3,9 @@ import axios from "axios";
 // axios.defaults.withCredentials = true;
 // axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: "http://64.226.119.110:8081",
+
   headers: {
     "Content-Type": "application/json",
   },
